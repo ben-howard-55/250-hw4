d64253873814a56bcb7ef7441f62f4c73aa56143
Ben Howard <62419637+ben-howard-55@users.noreply.github.com>
2020-05-06 16:48:14 +1200

    updated DFS.java to return DFS cycles

--- homework4/src/DFS.java
+++ homework4/src/DFS.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+<<<<<<< Updated upstream
 import java.util.Arrays;
+=======
+>>>>>>> Stashed changes
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -12,7 +15,6 @@ public class DFS {
     private Stack<Integer> _dfs_stack; 
     private ArrayList<Boolean> _dfs_explored;
     private ArrayList<LinkedList<Integer>> _graph;
-    private ArrayList<Degree<Integer, Integer>> _dfs_tree;
     private int _start;
     private ArrayList<ArrayList<Integer>> cycleList = new ArrayList<ArrayList<Integer>>();
 
@@ -22,13 +24,13 @@ public class DFS {
         _graph = graph;
         _dfs_stack = new Stack<Integer>();
         _dfs_explored = new ArrayList<Boolean>();
-        _dfs_tree = new ArrayList<Degree<Integer, Integer>>();
 
         for (int i = 0; i <graph.size(); i++) {
             _dfs_explored.add(false);
         }
 
         DFS_IMPL(s);
+<<<<<<< Updated upstream
        // cycleList.stream().distinct().collect(Collectors.toList());
         for (int i = 0; i < cycleList.size(); i++) {
             Collections.sort(cycleList.get(i));
@@ -38,12 +40,26 @@ public class DFS {
 
     public ArrayList<ArrayList<Integer>> returnCycles() {
         return cycleList;
+=======
+
+        for (int i = 0; i < cycleList.size(); i++) {
+            Collections.sort(cycleList.get(i));
+        }
+
+>>>>>>> Stashed changes
     }
 
  
 
+<<<<<<< Updated upstream
     
     
+=======
+    public ArrayList<ArrayList<Integer>> returnCycles() {
+        return cycleList;
+    }
+
+>>>>>>> Stashed changes
     private void DFS_IMPL(int s) {
         _dfs_stack.push(s);
         _dfs_explored.set(s, true);
@@ -57,7 +73,6 @@ public class DFS {
                 int nextVertex = vList.next();
 
                 if(_dfs_explored.get(nextVertex) == false ) {
-                    _dfs_tree.add(new Degree<Integer, Integer> (vertex, nextVertex));
                     DFS_IMPL(nextVertex);
                 } else if (_start == nextVertex) {
                    cycleList.add( new ArrayList<>(_dfs_stack));
